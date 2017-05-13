13651d232ceaa843ef7e4559b0e69f1f883bddcf
Mikez_ios <lee727n@aliyun.com>
2017-05-13 20:11:09 +0800

    add new comment
    Add a new comment  to  help you  understand this program

--- bit mouse/打地鼠/Mouse.m
+++ bit mouse/打地鼠/Mouse.m
@@ -15,7 +15,7 @@
     self = [super initWithFrame:frame];
     if (self) {
         self.backgroundColor = [UIColor redColor];
-        //添加背景图片
+        //添加背景图片 按钮背景图片 如果使用setimage 数字将无法显示在图片上层
         [self setBackgroundImage:[UIImage imageNamed:@"11"] forState:UIControlStateNormal];
       
         [self addTarget:self action:@selector(clicked) forControlEvents:UIControlEventTouchUpInside];
